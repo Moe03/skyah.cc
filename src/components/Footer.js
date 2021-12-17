088983f99980aa411fa34b71d7c0c1d07969b704
Moe Ayman <56455612+Moe03@users.noreply.github.com>
2021-12-17 23:41:30 +0200

    Updated API footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ class Footer extends Component {
         return (
             <div className="footer">
                 <div id="foot">
-                    Created by <a className="mlr" href="https://twitter.com/Mlotov_">Mlotov. </a>Not Affiliated with Hypixel or Mojang.
+                    Created by<a className="mlr" href="https://twitter.com/Mlotov_">Moe</a>| API powered by <a target="_blank" href='https://PurpleHosting.fr'>PurpleHosting.fr</a> | Not Affiliated with Hypixel.
                 </div>
                 <div className="t-c">
                     <Link to="/info"><a href="#info">About</a></Link> | <a target="_blank" href="https://discord.gg/eFmgTcr"> Discord</a>.
